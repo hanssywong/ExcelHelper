21424c9b42ad193a052d6597567f82a27e8b22bb
hanswong <31908287+hanssywong@users.noreply.github.com>
2023-10-06 15:57:01 +0800

    Init Project

---
+++ ExcelHelper/BaseExcel.cs
@@ -0,0 +1,50 @@
+﻿using Serilog;
+using Spire.Xls;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelHelper
+{
+    public class BaseExcel
+    {
+        public BaseExcel(string path, ILogger logger)
+        {
+            Logger = logger;
+            Path = path;
+            Wb.Worksheets.Clear();
+        }
+        ~BaseExcel()
+        {
+            Wb.Dispose();
+        }
+        public ILogger? Logger { get; protected set; }
+        public string Path { get; protected set; }
+        public Workbook Wb { get; protected set; } = new();
+        public enum Region
+        {
+            MY,
+            SG
+        }
+        public void AddSheet<T>(string sheetname, ImmutableList<T> data) where T : class
+        {
+            Worksheet? sheet1 = Wb.Worksheets[sheetname];
+            if (sheet1==null)
+            {
+                sheet1 = Wb.Worksheets.Add(sheetname);
+            }
+            sheet1.InsertDataTable(data.ToDataTable(sheetname), true, 1, 1);
+        }
+        public void Publish2Excel()
+        {
+            Wb.SaveToFile(Path, ExcelVersion.Version2013);
+        }
+        public void ReadFromExcel()
+        {
+            Wb.LoadFromFile(Path);
+        }
+    }
+}
---
+++ ExcelHelper/ImmutableListDI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelHelper
+{
+    public static class ImmutableListDI
+    {
+        public static DataTable ToDataTable<T>(this ImmutableList<T> items, string tablename) where T : class
+        {
+            DataTable table = new(tablename);
+            foreach(var item in items)
+            {
+                var properties = item.GetType().GetProperties();
+                if (table.Columns.Count == 0)
+                {
+                    foreach (var property in properties)
+                    {
+                        table.Columns.Add(property.Name, Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType);
+                    }
+                }
+                DataRow row = table.NewRow();
+                foreach (var property in properties)
+                {
+                    row[property.Name] = property.GetValue(item) ?? DBNull.Value;
+                }
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+    }
+}
